a1fb0c1eec5278bf400d0efc26e1222855917127
samiratou boro <samiratouboro@LAPTOP-EDAQPRB9.guestdomain>
2025-05-04 18:17:51 -0400

    remove real backet name

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "terraformstate-ska"
+    bucket       = "replace here with your backet name"
     key          = "alb/terraform.state"
     region       = "us-east-1"
     encrypt      = true
